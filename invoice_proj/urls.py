80b8c09dfee5fefd88fa191d0415ff3437e464fc
Hiradoras <forwoermind@gmail.com>
2022-02-21 16:57:43 +0600

    Added static and static media confs

--- invoice_proj/urls.py
+++ invoice_proj/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 #from .views import hello_world
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
  #   path('',hello_world)
 ]
+
+# urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Invoice admin system'
 admin.site.index_title = 'Managment'
