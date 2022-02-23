fa02ecd37989abeb6ce6c42ca028831edddd6335
Hiradoras <forwoermind@gmail.com>
2022-02-23 15:02:14 +0600

    defined a function for showing only logged in user's invoices.

--- invoices/views.py
+++ invoices/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Invoice
-
+from profiles.models import Profile
 
 class InvoiceListView(ListView):
     model = Invoice
-    # template_name = "invoices/main.html" # default invoice_list.html
+    template_name = "invoices/main.html" # default invoice_list.html
     # paginate_by
-    # context_object_name = 'qs'
+    context_object_name = 'qs'
+
+    def get_queryset(self):
+        # profile = Profile.objects.get(user=self.request.user)
+        # With doing this, you can show logged profile only
+        profile = get_object_or_404(Profile, user = self.request.user)
+        # qs = Invoice.objects.filter(profile=profile).order_by("-created")
+        # return qs
+        return super().get_queryset().filter(profile=profile).order_by('-created')
